3f7c659401aabdb8eefd59fcb2ff6349be85dbd7
Thendo Rambane <rambane.t.r@gmail.com>
2021-07-05 23:30:52 +0200

    remove css options

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,3 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === "production" ? "/sondi-frontend/" : "/",
-  css: {
-    loaderOptions: {
-      sass: {
-        additionalData: '@import "@/scss/main.scss";',
-      },
-    },
-  },
 };
