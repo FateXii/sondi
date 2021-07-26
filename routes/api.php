209b539f2e6e0740070907c5aeaceea4221f125b
FateXii <rambane.t.r@gmail.com>
2021-07-26 15:38:22 +0200

    Add new routes

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,12 @@
 <?php
 
+use App\Http\Controllers\AddressController;
 use App\Http\Controllers\PhotoController;
 use App\Http\Controllers\PropertyController;
 use App\Http\Controllers\RentalsController;
 use App\Http\Controllers\SalesController;
+use App\Http\Controllers\SectionalsController;
+use App\Http\Controllers\SectionalUnitController;
 use App\Models\Address;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -20,7 +23,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+  return $request->user();
 });
 
 Route::apiResource(
@@ -39,11 +42,21 @@ Route::apiResource(
 );
 
 Route::apiResource(
-  'addresses',
-  Address::class
-);
-
-Route::apiResource(
   'rentals',
   RentalsController::class
 );
+
+Route::apiResource(
+  'sectionals',
+  SectionalsController::class
+);
+
+Route::apiResource(
+  'address',
+  AddressController::class
+);
+
+Route::apiResource(
+  'sectionals.unit',
+  SectionalUnitController::class
+)->shallow();
