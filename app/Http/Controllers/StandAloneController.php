f31affb73d4a4c04b22c007f0b74016db4eace18
FateXii <rambane.t.r@gmail.com>
2021-08-31 14:21:50 +0200

    Implement standalone property

--- app/Http/Controllers/StandAloneController.php
+++ app/Http/Controllers/StandAloneController.php
@@ -4,61 +4,71 @@ namespace App\Http\Controllers;
 
 use App\Models\StandAlone;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use PhpParser\PrettyPrinter\Standard;
 
 class StandAloneController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index()
+  {
+    return StandAlone::all();
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(Request $request)
+  {
+    $standAlone = new StandAlone;
+    $standAlone->addresses_id = $request->addresses_id;
+    $standAlone->save();
+    return $standAlone;
+  }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\StandAlone  $standAlone
-     * @return \Illuminate\Http\Response
-     */
-    public function show(StandAlone $standAlone)
-    {
-        //
+  /**
+   * Display the specified resource.
+   *
+   * @param  \App\Models\StandAlone  $standAlone
+   * @return \Illuminate\Http\Response
+   */
+  public function show(StandAlone $standAlone)
+  {
+    if (!$standAlone) {
+      return new Response([
+        'message' => 'property not found'
+      ], 404);
     }
+    return $standAlone;
+  }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\StandAlone  $standAlone
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, StandAlone $standAlone)
-    {
-        //
-    }
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  \App\Models\StandAlone  $standAlone
+   * @return \Illuminate\Http\Response
+   */
+  public function update(Request $request, StandAlone $standAlone)
+  {
+    //
+  }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\StandAlone  $standAlone
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(StandAlone $standAlone)
-    {
-        //
-    }
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  \App\Models\StandAlone  $standAlone
+   * @return \Illuminate\Http\Response
+   */
+  public function destroy(StandAlone $standAlone)
+  {
+    $standAlone->delete();
+  }
 }
