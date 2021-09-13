963ed77cdf3c9a15f2137626cacb9f9821d1e90d
Thendo Rambane <rambane.t.r@gmail.com>
2021-09-13 13:25:41 +0200

    Remove Service Worker for SPA

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === "production" ? "/sondi-frontend/" : "/",
+  pwa: {
+    iconPaths: {
+      faviconSVG: null,
+      favicon32: null,
+      favicon16: null,
+      appleTouchIcon: null,
+      maskIcon: null,
+      msTileImage: null,
+    },
+  },
 };
