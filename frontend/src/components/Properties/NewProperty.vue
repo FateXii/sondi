63e819b0cf6c0015da0d1f9f623cb039fe36a3de
Thendo Rambane <rambane.t.r@gmail.com>
2021-09-21 09:57:24 +0200

    Remove unnecessary markup

--- frontend/src/components/Properties/NewProperty.vue
+++ frontend/src/components/Properties/NewProperty.vue
@@ -1,36 +1,30 @@
 <template>
-  <el-container
-    class="property-detail"
-    id="affix-container"
-    v-loading="!property"
-  >
-    <div class="property">
-      <el-form label-position="top">
-        <div class="property__heading">
-          <el-form-item label="Title">
-            <el-input />
-          </el-form-item>
-          <el-form-item label="Cover Image">
-            <single-image-upload @onImageChange="handleCoverImageChange" />
-          </el-form-item>
-        </div>
-        <div class="property__images property-item">
-          <!-- <carousel /> -->
-        </div>
-        <div class="property__description property-item">
+  <div class="property">
+    <el-form label-position="top">
+      <div class="property__heading">
+        <el-form-item label="Title">
+          <el-input />
+        </el-form-item>
+        <el-form-item label="Cover Image">
+          <single-image-upload @onImageChange="handleCoverImageChange" />
+        </el-form-item>
+      </div>
+      <div class="property__images property-item">
+        <!-- <carousel /> -->
+      </div>
+      <div class="property__description property-item">
         <property-description-form />
-        </div>
-        <div class="property__features property-item">
-          <el-form-item label="Features">
-            <property-feature-list />
-          </el-form-item>
-        </div>
-        <div class="property__agents property-item">
-          <el-form-item> </el-form-item>
-        </div>
-      </el-form>
-    </div>
-  </el-container>
+      </div>
+      <div class="property__features property-item">
+        <el-form-item label="Features">
+          <property-feature-list />
+        </el-form-item>
+      </div>
+      <div class="property__agents property-item">
+        <el-form-item> </el-form-item>
+      </div>
+    </el-form>
+  </div>
 </template>
 
 <script lang="ts">
@@ -38,7 +32,6 @@ import { defineComponent, ref } from "vue";
 import GetScreenWidth from "@/Helpers/GetScreenWidth";
 import { NewProperty } from "@/composables/Properties/NewProperty";
 import SingleImageUpload from "@/components/Properties/NewProperty/SingleImageUpload.vue";
-import PropertyAddress from "@/components/Properties/NewProperty/PropertyAddress.vue";
 import PropertyFeatureList from "./NewProperty/PropertyFeatures/PropertyFeatureList.vue";
 import PropertyDescriptionForm from "./NewProperty/PropertyDescriptionForm.vue";
 import { titleCase } from "@/Helpers";
@@ -80,35 +73,21 @@ export default defineComponent({
   padding: 1.75rem;
 }
 
-.property-detail {
-  display: grid;
-  gap: 1rem;
-  padding: 2rem;
-  @media (max-width: 767px) {
-    display: flex;
-    flex-flow: column;
-    padding: 0;
-  }
-  .contact {
-    flex: 1;
-    height: fit-content;
-  }
-  .property {
-    height: fit-content;
-    flex: 2;
-    &__agents {
-      &__item {
-        &__details {
-        }
+.property {
+  height: fit-content;
+  flex: 2;
+  &__agents {
+    &__item {
+      &__details {
       }
     }
-    &__heading {
-    }
-    &__description {
-    }
-    &__images {
-      &__image {
-      }
+  }
+  &__heading {
+  }
+  &__description {
+  }
+  &__images {
+    &__image {
     }
   }
 }
