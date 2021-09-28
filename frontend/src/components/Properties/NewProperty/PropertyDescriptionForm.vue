edc8fa1c23ad327b255c21a946dfe16953e49e35
Thendo Rambane <rambane.t.r@gmail.com>
2021-09-28 11:50:15 +0200

    Capture and emit property description data

--- frontend/src/components/Properties/NewProperty/PropertyDescriptionForm.vue
+++ frontend/src/components/Properties/NewProperty/PropertyDescriptionForm.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="property__description__title">
     <el-form-item label="Description Title">
-      <el-input />
+      <el-input v-model="property.title" />
     </el-form-item>
   </div>
   <div class="property__decription__price">
     <el-form-item label="Price">
-      <el-input />
+      <el-input v-model="property.price" />
     </el-form-item>
   </div>
   <div class="property__decription_address">
@@ -16,31 +16,58 @@
   </div>
   <h3 class="property__description__heading">
     <el-form-item label="Description Heading">
-      <el-input />
+      <el-input v-model="property.description.heading" />
     </el-form-item>
   </h3>
   <div class="property__description__text">
     <el-form-item label="Description">
-      <el-input type="textarea" />
+      <el-input type="textarea" v-model="property.description.text" />
     </el-form-item>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, reactive, watch } from "vue";
 import PropertyAddress from "@/components/Properties/NewProperty/PropertyAddress.vue";
-import { IAddressForm } from "@/interfaces";
+import { IAddressForm, IPropertyDescriptionForm } from "@/interfaces/Forms";
 
 export default defineComponent({
   components: {
     PropertyAddress,
   },
-  setup() {
+  emits: {
+    descriptionFormChange: (form: IPropertyDescriptionForm) =>
+      (form && true) || false,
+  },
+  setup(_, { emit }) {
+    const property = reactive<IPropertyDescriptionForm>({
+      title: String(),
+      price: Number(),
+      address: {
+        isSectional: Boolean(),
+        sectionalId: Number(),
+        address: {
+          unit: String(),
+          street: String(),
+          city: String(),
+          province: String(),
+          postal_code: String(),
+        },
+      },
+      description: {
+        text: String(),
+        heading: String(),
+      },
+    });
     function addressChange(addressFormData: IAddressForm) {
-      console.log(addressFormData);
+      Object.assign(property.address, addressFormData);
     }
+    watch(property, (propertyForm) => {
+      emit("descriptionFormChange", propertyForm);
+    });
     return {
       addressChange,
+      property,
     };
   },
 });
