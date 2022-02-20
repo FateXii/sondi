a2d965dd5af19f3aaaea167a6cf63459ad7dc9e0
Thendo Rambane <rambane.t.r@gmail.com>
2022-02-21 01:03:39 +0200

    Add Featured property component

---
+++ frontend/src/components/FeaturedProperty.vue
@@ -0,0 +1,19 @@
+<template lang="html">
+  <el-image :src="property.cover_image" fit="cover"></el-image>
+</template>
+<script lang="ts">
+import { IProperty } from "@/Types/Property";
+import { defineComponent, PropType } from "vue";
+
+export default defineComponent({
+  props: {
+    property: {
+      type: Object as PropType<IProperty>,
+      required: true,
+    },
+  },
+  setup() {
+    return {};
+  },
+});
+</script>
